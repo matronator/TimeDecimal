b5c2dce2d32f3d2d2dc7d1ad9678cfcbaad24c0d
Matej Bystricky <info@matronator.com>
2020-03-01 04:04:44 +0100

    formulas

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 function calculateHours(h, m = 0, s = 0) {
+  return h + (m * (1 / 60)) + (s * 1 / 3600);
+}
 
+function calculateMinutes(h = 0, m, s = 0) {
+  return (h * (60 / 1)) + m + (s * 1 / 60);
+}
+
+function calculateSeconds(h = 0, m = 0, s) {
+  return (h * (3600 / 1)) + (m * 60 / 1) + s;
 }
