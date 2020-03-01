1d5baec3c2668c277e4fc9e6c7dda8dd099b90ac
Matej Bystricky <info@matronator.com>
2020-03-01 05:27:39 +0100

    done

---
+++ src/js/ui.js
@@ -0,0 +1,45 @@
+// Inputs
+const hi = document.getElementById("hours");
+const mi = document.getElementById("minutes");
+const si = document.getElementById("seconds");
+
+// Outputs
+var ho = document.getElementById("out-h");
+var mo = document.getElementById("out-m");
+var so = document.getElementById("out-s");
+
+// Calculate
+function calculateAll() {
+  ho.innerHTML = calculateHours(parseInt(hi.value), parseInt(mi.value), parseInt(si.value));
+  mo.innerHTML = calculateMinutes(parseInt(hi.value), parseInt(mi.value), parseInt(si.value));
+  so.innerHTML = calculateSeconds(parseInt(hi.value), parseInt(mi.value), parseInt(si.value));
+}
+
+// Change
+hi.addEventListener("input", function(){
+  let thisval = this.value;
+  if (/^[0-9]*$/.test(thisval)) {
+    calculateAll();
+  } else {
+    this.value = "0";
+  }
+});
+mi.addEventListener("input", function(){
+  let thisval = this.value;
+  if (/^[0-9]*$/.test(thisval)) {
+    calculateAll();
+  } else {
+    this.value = "0";
+  }
+});
+si.addEventListener("input", function(){
+  let thisval = this.value;
+  if (/^[0-9]*$/.test(thisval)) {
+    calculateAll();
+  } else {
+    this.value = "0";
+  }
+});
+
+// Init
+calculateAll();
